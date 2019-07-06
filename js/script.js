ae648de24aea273d639e3e5de4abc88cce68d3cd
Pawel Kowalewski <pawelkowalewski@hotmail.com>
2019-07-06 22:28:49 +0200

    add js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+let computerMove = 'kamień';
+let playerMove = "papier";
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
